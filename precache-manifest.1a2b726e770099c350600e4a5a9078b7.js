c8b66e650025bee01ab36b0fae34acac4f1dfa12
nicolo cervo <nicolo.cervo@gmail.com>
2020-09-17 20:31:20 +0200

    Updates

---
+++ precache-manifest.1a2b726e770099c350600e4a5a9078b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e6ca0c55db3a962ea51",
+    "url": "./static/css/main.b61f9158.chunk.css"
+  },
+  {
+    "revision": "5e6ca0c55db3a962ea51",
+    "url": "./static/js/main.5e6ca0c5.chunk.js"
+  },
+  {
+    "revision": "474eafbe39334d316004",
+    "url": "./static/css/1.bacb7a33.chunk.css"
+  },
+  {
+    "revision": "474eafbe39334d316004",
+    "url": "./static/js/1.474eafbe.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "./static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "7d1b47f5da0839e2de9ac83a224503fd",
+    "url": "./index.html"
+  }
+];
